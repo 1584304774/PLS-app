983804621ebd0915dc4a77c1477ba20cda3817d8
zym <1584304774@qq.com>
2019-02-22 12:17:21 +0800

    主页修改

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 window.onload = function(){
-	//显示个人注册信息
 	var $portrait = $("#portrait");
 	var $jiahao = $(".icon-jiahao");
 	var $plusSmall = $("#plusSmall");
 	var plus = document.getElementById("plus");
-	$portrait.on("click",function(){
-		location.href = "personal.html";
+	//点击头像显示个人注册信息
+	$portrait.click(function(){
+		
+		$("#personal").animate({left : 0},1000);
+		return false;
 	})
 	//点击加号，一秒内将div显示，点击其他地方隐藏
 	$jiahao.on("click",function(){
@@ -14,6 +16,16 @@ window.onload = function(){
 	})
 	$(document).on("click",function(){
 		$plusSmall.slideUp(1000);
+		$("#personal").animate({left : "-2.63rem"},1000)
 	})
+	//选项卡功能,点击footer中的div显示相应的section
+	$("footer div").click(function(){
+		$(this).addClass("active").siblings().removeClass("active");
+		var index = $(this).index();
+		$("section").eq(index).show().siblings().hide();
+		$("footer").css("display","block");//点击时会将footer隐藏，所以需要将footer显示
+		$(this).parent().parent().prev().find("h2").html($(this).find("em").html());
+	})
+	
 }
 
--- js/start.js
+++ js/start.js
@@ -2,14 +2,12 @@ window.onload = function(){
 	var $register = $("#register");
 	var $login = $("#login");
 	var $img = $("img");
-	var op = 10;
 	//背景虚化
 	setInterval(function(){
-		$img.animate({opacity : (op--)/10},1000)
-		if(op/10 < 0.6){
-			$img.animate({opacity : (op++)/10},500)
-		}
-	},1000);
+		$img.animate({opacity : 0.4},2000,function(){
+			$img.animate({opacity : 0.8},2000)
+		})
+	},5000);
 	$register.on("click",function(){
 		location.href = "register.html";
 	})
